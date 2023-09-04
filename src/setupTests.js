794abc2c35985e365df027b41736d4a4d6460e28
Mustaghees Butt <renaldpirate@gmail.com>
2023-09-04 15:31:26 +0500

    fixed tests + upgrade to React 18

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,9 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-import 'jest-canvas-mock'
+import { afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
+import 'vitest-canvas-mock'
+
+// runs a cleanup after each test case (e.g. clearing jsdom)
+afterEach(() => {
+  cleanup();
+});
